6f3b48cce221a0d737fde4af103d3f2252f6340b
Percocco <gabriele.percoco@demetrix.it>
2024-10-16 14:34:14 +0200

    update controller

--- Minio-SQL-connector/api/controllers/controller.js
+++ Minio-SQL-connector/api/controllers/controller.js
@@ -14,10 +14,10 @@ module.exports = {
         console.log(req.query, req.headers.visibility)
         //if (common.isRawQuery(req.query))
         //return res.send(await service.rawQuery(req.query, req.body.prefix, req.body.bucketName, req.headers.visibility))
-        let rawQuery = await service.rawQuery(req.query, req.body.prefix, req.body.bucketName, req.headers.visibility)
+        let rawQuery = await service.rawQuery({...req.query, format : "Object"}, req.body.prefix, req.body.bucketName, req.headers.visibility)
         if (rawQuery && !Array.isArray(rawQuery))
             rawQuery = [rawQuery]
-        let mongoQuery = await service.mongoQuery(req.query, req.body.prefix, req.body.bucketName, req.headers.visibility)
+        let mongoQuery = await service.mongoQuery({...req.query, format : "JSON"}, req.body.prefix, req.body.bucketName, req.headers.visibility)
         if (mongoQuery && !Array.isArray(mongoQuery))
             mongoQuery = [mongoQuery]
         if (rawQuery && mongoQuery)
