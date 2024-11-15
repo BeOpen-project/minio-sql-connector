01a1d78aa56a96ed71ee6a0bb721265d327bfb34
Gabriele <gabriele.percoco@demetrix.it>
2024-11-15 15:26:27 +0100

    patch

--- Minio-SQL-connector/api/controllers/controller.js
+++ Minio-SQL-connector/api/controllers/controller.js
@@ -33,12 +33,14 @@ module.exports = {
 
     querySQL: async (req, res, next) => {
         console.log("Query sql")
-        if (req.body.mongoQuery)
-            return next(req,res)
+        if (req.body.mongoQuery){
+            console.debug("Mongo query")
+            return await next(req,res)
+        }
         if (!req.body.query)
             return res.status(400).send("Missing query")
         const requestData = req.body.query;
-        console.log(requestData)
+        console.log("Query : ", requestData)
         service.querySQL(res, req.body.query, req.body.prefix, req.body.bucketName, req.headers.visibility)
     },
 
