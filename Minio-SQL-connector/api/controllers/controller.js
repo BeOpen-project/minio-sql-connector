04d5f8512f24b148f1e47899b488ece87c8db06f
Gabriele <gabriele.percoco@demetrix.it>
2024-11-15 15:42:32 +0100

    patch

--- Minio-SQL-connector/api/controllers/controller.js
+++ Minio-SQL-connector/api/controllers/controller.js
@@ -35,7 +35,32 @@ module.exports = {
         console.log("Query sql")
         if (req.body.mongoQuery){
             console.debug("Mongo query")
-            return await next(req,res)
+            console.log("Query mongo")
+        console.log(req.query, req.headers.visibility, req.body)
+        if (common.isRawQuery(req.query))
+            res.send(await service.rawQuery(req.query, req.body.prefix, req.body.bucketName, req.headers.visibility))
+        else {
+            if (req.query.format == "JSON") {
+                let objectQuerySet = JSON.parse(JSON.stringify(req.body.mongoQuery || req.query))
+                objectQuerySet.format = "Object"
+                let JSONQuerySet = JSON.parse(JSON.stringify(req.body.mongoQuery || req.query))
+                JSONQuerySet.format = "JSON"
+                let objectQuery = await service.mongoQuery(objectQuerySet, req.body.prefix, req.body.bucketName, req.headers.visibility)
+                if (objectQuery && !Array.isArray(objectQuery))
+                    objectQuery = [objectQuery]
+                let JSONQuery = await service.mongoQuery(JSONQuerySet, req.body.prefix, req.body.bucketName, req.headers.visibility)
+                if (JSONQuery && !Array.isArray(JSONQuery))
+                    JSONQuery = [JSONQuery]
+                if (JSONQuery && objectQuery)
+                    res.send(JSONQuery.concat(objectQuery))
+                else
+                    res.send(JSONQuery || objectQuery)
+            }
+            else
+                res.send(await service.mongoQuery(req.body.mongoQuery || req.query, req.body.prefix, req.body.bucketName, req.headers.visibility))
+        }
+        console.log("Query mongo finished")
+        return "Query mongo finished"
         }
         if (!req.body.query)
             return res.status(400).send("Missing query")
