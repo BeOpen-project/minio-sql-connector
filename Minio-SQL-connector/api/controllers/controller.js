1747dd4194bd7bb4cf07ec2756b144bc8cb3ab92
Gabriele <gabriele.percoco@demetrix.it>
2024-11-22 23:33:59 +0100

    fix date query

--- Minio-SQL-connector/api/controllers/controller.js
+++ Minio-SQL-connector/api/controllers/controller.js
@@ -33,34 +33,35 @@ module.exports = {
 
     querySQL: async (req, res, next) => {
         console.log("Query sql")
-        if (req.body.mongoQuery){
+        if (req.body.mongoQuery) {
             console.debug("Mongo query")
             console.log("Query mongo")
-        console.log(req.query, req.headers.visibility, req.body)
-        if (common.isRawQuery(req.query))
-            res.send(await service.rawQuery(req.query, req.body.prefix, req.body.bucketName, req.headers.visibility))
-        else {
-            if (req.query.format == "JSON") {
-                let objectQuerySet = JSON.parse(JSON.stringify(req.body.mongoQuery || req.query))
-                objectQuerySet.format = "Object"
-                let JSONQuerySet = JSON.parse(JSON.stringify(req.body.mongoQuery || req.query))
-                JSONQuerySet.format = "JSON"
-                let objectQuery = await service.mongoQuery(objectQuerySet, req.body.prefix, req.body.bucketName, req.headers.visibility)
-                if (objectQuery && !Array.isArray(objectQuery))
-                    objectQuery = [objectQuery]
-                let JSONQuery = await service.mongoQuery(JSONQuerySet, req.body.prefix, req.body.bucketName, req.headers.visibility)
-                if (JSONQuery && !Array.isArray(JSONQuery))
-                    JSONQuery = [JSONQuery]
-                if (JSONQuery && objectQuery)
-                    res.send(JSONQuery.concat(objectQuery))
+            console.log(req.query, req.headers.visibility, req.body)
+            if (common.isRawQuery(req.query))
+                res.send(await service.rawQuery(req.query, req.body.prefix, req.body.bucketName, req.headers.visibility))
+            else {
+                console.log("No raw query")
+                if (req.query.format == "JSON") {
+                    let objectQuerySet = JSON.parse(JSON.stringify(req.body.mongoQuery || req.query))
+                    objectQuerySet.format = "Object"
+                    let JSONQuerySet = JSON.parse(JSON.stringify(req.body.mongoQuery || req.query))
+                    JSONQuerySet.format = "JSON"
+                    let objectQuery = await service.mongoQuery(objectQuerySet, req.body.prefix, req.body.bucketName, req.headers.visibility)
+                    if (objectQuery && !Array.isArray(objectQuery))
+                        objectQuery = [objectQuery]
+                    let JSONQuery = await service.mongoQuery(JSONQuerySet, req.body.prefix, req.body.bucketName, req.headers.visibility)
+                    if (JSONQuery && !Array.isArray(JSONQuery))
+                        JSONQuery = [JSONQuery]
+                    if (JSONQuery && objectQuery)
+                        res.send(JSONQuery.concat(objectQuery))
+                    else
+                        res.send(JSONQuery || objectQuery)
+                }
                 else
-                    res.send(JSONQuery || objectQuery)
+                    res.send(await service.mongoQuery(req.body.mongoQuery || req.query, req.body.prefix, req.body.bucketName, req.headers.visibility))
             }
-            else
-                res.send(await service.mongoQuery(req.body.mongoQuery || req.query, req.body.prefix, req.body.bucketName, req.headers.visibility))
-        }
-        console.log("Query mongo finished")
-        return "Query mongo finished"
+            console.log("Query mongo finished")
+            return "Query mongo finished"
         }
         if (!req.body.query)
             return res.status(400).send("Missing query")
